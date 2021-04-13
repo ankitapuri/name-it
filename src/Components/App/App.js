17b09b7b4c383aa10f1db0ff0e294889bb27ceaf
Ankita Puri <54734002+ankitapuri@users.noreply.github.com>
2021-04-13 05:20:22 +0000

    first component

---
+++ src/Components/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class App extends React.Component{
+  render(){
+    return  <h1>This is functional component</h1>;
+  }
+}
+
+
+// function App() {
+//   return (
+   
+//   );
+// }
+
+ export default App;
