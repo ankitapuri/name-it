1ba62e6081c6ff69a2ad31216010486c3b509eb7
Ankita Puri <54734002+ankitapuri@users.noreply.github.com>
2021-04-14 19:57:59 +0000

    states and props

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Header.css'
-const header = () =>{
+const header = (props) =>{     // here the props is passed as the argument in the fuction declared
     return (
         <div className="head-container">
             <img src='https://user-images.githubusercontent.com/26179770/106359099-693e3380-6336-11eb-8069-9e36f25de5ca.png'
              className="head-image"
-             alt="headerImage"/>
-            <h1 className="head-text">Name-it!</h1>
+             alt="headerImage"/>      
+            <h1 className="head-text">{props.headtitle}</h1>  {/*  calling that prop , it avoids hard-coding it */ }
         </div>
     );
 };
