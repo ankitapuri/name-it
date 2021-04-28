bcf78b01eceebb26a008ab1528f3b32f9ece59bf
Ankita Puri <puriankita30@gmail.com>
2021-04-28 12:16:37 +0000

    added animation

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,10 +5,19 @@ const header = ({headtitle, headerExpanded}) => {
     return (
         <div className="head-container">
             <img
-                src="https://user-images.githubusercontent.com/26179770/106359099-693e3380-6336-11eb-8069-9e36f25de5ca.png"
-                className={`head-image${headerExpanded ? 'head-image-expanded':'head-image-contracted'}`}
-                alt="headerImage"/>
-            <h1 className="head-text">{headtitle}</h1>{' '}
+                src='https://user-images.githubusercontent.com/26179770/106359099-693e3380-6336-11eb-8069-9e36f25de5ca.png'
+                className={`head-image ${
+                    headerExpanded
+                        ? 'head-image-expanded'
+                        : 'head-image-contracted'
+                }`}
+                alt="header"
+            />
+            <h1 className={`head-text ${
+                    headerExpanded
+                        ? 'head-text-expanded'
+                        : 'head-text-contracted'
+                }`}>{headtitle}</h1>
             {/*  calling that prop , it avoids hard-coding it */}
             
         </div>
